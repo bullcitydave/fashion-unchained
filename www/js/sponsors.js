6bbe31630ea7fbce87d52639070025b3be3959a0
Dave Seidman <bullcitydave@gmail.com>
2016-04-24 12:42:26 -0400

    Updated content/sponsors

---
+++ www/js/sponsors.js
@@ -0,0 +1,52 @@
+var goldsponsors = [
+  { 
+    "alt" : "Yelp",
+    "imageUrl" : "https://www.unchaindogs.net/wp-content/uploads/yelp1.jpg"
+  },
+  { 
+    "alt" : "Gailor",
+    "imageUrl" : "https://www.unchaindogs.net/wp-content/uploads/Gailor.jpg"
+  },
+  { 
+    "alt" : "Magnolia Animal Hospital",
+    "imageUrl" : "https://www.unchaindogs.net/wp-content/uploads/Magnolia-Animal-Hospital.jpg"
+  },
+  {
+  "alt" : "James Scott Ferrin",
+  "imageUrl" : "https://doc-04-1k-docs.googleusercontent.com/docs/securesc/bqg97j9mrkvqnrmnn1ngn68ovngoac15/fvooqduqulcvp3p1h32p2hntt1qq90k6/1461506400000/08545512234961362459/08545512234961362459/0B4gKlOWN02OTdlFzNi00eURIME9GTFJIRU1WN0JCTFE4aDRv?e=view&nonce=i5b6f9s5sp7g2&user=08545512234961362459&hash=3d5oatkv85dl7s3hoet5mgmau3m5cvam"
+ },
+ {
+  "alt" : "Models For Charity",
+  "imageUrl" : "http://i.imgur.com/IcQfIeR.png"
+ },
+ {
+  "alt" : "Ferret Sound",
+  "imageUrl" : "http://i.imgur.com/olwUIUF.png"
+ },
+ {
+  "alt" : "Big Boss",
+  "imageUrl" : "https://drive.google.com/uc?export=view&id=0B4gKlOWN02OTNGh0TU1zZEhoQTZUaGp1akozZ1VwQ0pVRDI0"
+ }
+
+];
+
+var diamondsponsors = [
+  {
+    "alt" : "Roscoe\'s Remodeling",
+    "imageUrl" : "http://i.imgur.com/DejcLLT.png"
+  },
+  { 
+    "alt" :"The Triangle Dog Magazine",
+    "imageUrl" : "http://i.imgur.com/JKT9cNX.png"}
+  ];
+
+var boutiques = [
+  {
+    "alt" : "Adore",
+    "imageUrl" : "https://drive.google.com/uc?export=view&id=0B4gKlOWN02OTeU5mbFJSS0dVVFFPMG9wYjJkaFNXSGdTMWtN"
+  },
+  { 
+    "alt" :"Galatea",
+    "imageUrl" : "https://drive.google.com/uc?export=view&id=0B4gKlOWN02OTbDhmcm9zZC1CdUVLYnFqOW9Ja0h5OFBtM3g0"
+  }];
+
